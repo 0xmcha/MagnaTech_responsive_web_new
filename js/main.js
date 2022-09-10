11a8370603d05e085bf3c0b3b3c9252237081dc7
mir-barzan <meer0770@gmail.com>
2022-09-10 04:14:59 +0300

    some changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
+<<<<<<< Updated upstream
 // refernce
     var messagesRef = firebase.database().ref('messages');
 
 
+=======
+var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
+const firebaseConfig = {
+    apiKey: "AIzaSyAbv6MER0tymIAb7TOqMHyqcJ78tNU64Dg",
+    authDomain: "magnatech-8d29d.firebaseapp.com",
+    databaseURL: "https://magnatech-8d29d-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "magnatech-8d29d",
+    storageBucket: "magnatech-8d29d.appspot.com",
+    messagingSenderId: "30108335069",
+    appId: "1:30108335069:web:a2ce72530d936248385434",
+    
+  };
+  //initialize firebase
+  const app = initializeApp(firebaseConfig);
+
+  //Reference messages collection
+  let messagesRef = firebase.firestor().ref('contactForm');
+  
+>>>>>>> Stashed changes
   //listen to form
   document.getElementById('contactForm').addEventListener('submit', submitForm);
  
